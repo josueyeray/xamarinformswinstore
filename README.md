0a9689ec8e42770f8626b4ba20e490481051ffd8
josueyeray <josueyeray@gmail.com>
2015-03-22 11:44:04 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # xamarinformswinstore
-Little sample of Xamarin Forms new preview for Windows Store.
+Little sample of Xamarin Forms new preview for Windows Store. You can read the article in spanish:
+http://bit.ly/xfwsES
+
+English version comming up...
---
+++ XamarinForWinStore/XamarinForWinStore/App.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XamarinForWinStore
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new ContentPage
+            {
+                BackgroundColor = Color.FromRgba(0.3, 0.3, 0.5, 1),
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children = {
+						new Label {
+							XAlign = TextAlignment.Center,
+							Text = "Welcome to Xamarin Forms! on WinRT",
+                            FontSize = 72
+						}
+					}
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
